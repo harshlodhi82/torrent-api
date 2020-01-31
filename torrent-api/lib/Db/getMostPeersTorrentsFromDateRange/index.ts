d58ac2df7e0d9caed51ca8c444e69bef9f897e16
Harsh Lodhi <harshlodhi82@gmail.com>
2020-01-31 23:02:16 +0530

    Fixed all discussed issues

--- torrent-api/lib/Db/getMostPeersTorrentsFromDateRange/index.ts
+++ torrent-api/lib/Db/getMostPeersTorrentsFromDateRange/index.ts
@@ -10,13 +10,13 @@ interface GetMostPeersTorrentsFromDateRange {
 
 // eslint-disable-next-line require-await
 const getMostPeersTorrentsFromDateRange: GetMostPeersTorrentsFromDateRange = async function ({startDate, endDate, limit}) {
-  let allDates = await this.sequelize.query(`SELECT DISTINCT torrents.infohash FROM dates
+  let mostPeersInfohash = await this.sequelize.query(`SELECT DISTINCT torrents.infohash FROM dates
   JOIN torrents on dates.infohash = torrents.infohash
   where date(dates.date) >= '${startDate}' AND date(dates.date) <= '${endDate}'
   ORDER BY dates.peers DESC LIMIT ${limit}`)
   const attributes = ['name', 'size', 'categories', 'createdAt', 'files']
   let data = await this.Torrent.findAll({
-    where: {[this.sequelize.Sequelize.Op.or]: allDates[0]},
+    where: {[this.sequelize.Sequelize.Op.or]: mostPeersInfohash[0]},
     attributes,
     include: [{
       model: this.Date,
--- torrent-api/lib/server/routes/index.ts
+++ torrent-api/lib/server/routes/index.ts
@@ -12,27 +12,33 @@ router.get('/time', (req, res) => {
 })
 
 router.get('/torrent/:torrentName', async (req, res) => {
-  const db = req.app.get('db')
-  const torrentName = req.params.torrentName
-  if (!torrentName) throw Error('No name provided')
-  const torrent = await db.getTorrentByName(torrentName)
-  res.json(torrent)
+  try {
+    const db = req.app.get('db')
+    const torrentName = req.params.torrentName
+    if (!torrentName) throw Error('No name provided')
+    const torrent = await db.getTorrentByName(torrentName)
+    res.json(torrent)
+  }
+  catch (error) {
+    res.status(404).send(error)
+  }
 })
 
 router.get('/torrents', async (req, res) => {
-  const db = req.app.get('db')
-  let startDate = req.query.startDate
-  let endDate = req.query.endDate
-  let limit = req.query.limit
-
-  const todayDate = new Date(Date.now()).toISOString().split('T')[0]
-  const sevenDayBeforeDate = new Date(Date.now() - (7 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0]
-  startDate = (startDate) || sevenDayBeforeDate
-  endDate = (endDate) || todayDate
-  limit = (limit) || 100
-
-  const peerDates = await db.getMostPeersTorrentsFromDateRange({startDate, endDate, limit})
-  res.json(peerDates)
+  try {
+    const db = req.app.get('db')
+    let {startDate, endDate, limit} = req.query
+    const todayDate = new Date(Date.now()).toISOString().split('T')[0]
+    const sevenDayBeforeDate = new Date(Date.now() - (7 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0]
+    startDate = (startDate) || sevenDayBeforeDate
+    endDate = (endDate) || todayDate
+    limit = (limit) || 100
+    const peerDates = await db.getMostPeersTorrentsFromDateRange({startDate, endDate, limit})
+    res.json(peerDates)
+  }
+  catch (error) {
+    res.status(404).send(error)
+  }
 })
 
 router.all('*', send404)
